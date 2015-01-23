9c7d4b223a25a6e3c0343d665454e5adf1738f75
Rafael Angelo <rlangelo@wpi.edu>
2015-01-22 20:46:51 -0500

    *sighs*

--- src/connectN/Player.java
+++ src/connectN/Player.java
@@ -1,13 +1,9 @@
 package connectN;
 import java.io.*;
-
 import java.util.Arrays;
 import java.util.List;
 
 import refereePack.Board;
-
-
-
 
 public class Player {
 	
@@ -15,35 +11,64 @@ public class Player {
  BufferedReader input = new BufferedReader(new InputStreamReader(System.in));  
  boolean first_move=false;
  
- static int boardHeight, boardWidth, N;
+ static int boardHeight, boardWidth, N, playerNumber;
  
- Board board;
- 
+ int board[][];
  
  public void processInput() throws IOException{
  
   String s=input.readLine();
   List<String> ls=Arrays.asList(s.split(" "));
-  if(ls.size()==2){
-   
+  if(ls.size()==5){          //ls contains game info
+   boardWidth = Integer.parseInt(ls.get(1));
+   boardHeight = Integer.parseInt(ls.get(0));
+   playerNumber = Integer.parseInt(ls.get(3));
+   N = Integer.parseInt(ls.get(2));
+   System.out.println(Integer.toString(boardWidth/2) +" "+ "1");  //first move
+   createBoard(boardWidth, boardHeight);
+  }
+  else if(ls.size()==2){ 
+	 System.out.println(ls.get(0)+" "+ls.get(1));
+	 addToBoard(ls.get(0));
   }
   else if(ls.size()==1){
    System.out.println("game over!!!");
   }
-  else if(ls.size()==5){          //ls contains game info
-   boardWidth = Integer.parseInt(ls.get(1));
-   boardHeight = Integer.parseInt(ls.get(0));
-   N = Integer.parseInt(ls.get(2));
-   board = new Board(boardWidth, boardHeight, N);  
-   System.out.println(Integer.toString(boardWidth/2) +" "+ "1");  //first move
-   board.dropADiscFromTop(boardWidth/2, Integer.parseInt(ls.get(3)));
-   
-  }
   else if(ls.size()==4){  //player1: aa player2: bb
    //TODO combine this information with game information to decide who is the first player
   }
-  else
+  else 
    System.out.println("not what I want");  }
+  
+ 
+ public void addToBoard(String column) throws IOException {
+		int col = Integer.parseInt(column);
+		for (int i=0;i<boardHeight;i++)
+		{
+			if(board[i][col]==9)
+			{
+				if (playerNumber == 1)
+				{
+					board[i][col] = 2;
+				}
+				else
+				{
+					board[i][col] = 1;
+				}
+			}
+		}
+	}
+
+public void createBoard(int width, int height)
+{
+	   for (int i=0;i<width;i++)
+	   {
+		   for (int j=0;j<height;j++)
+		   {
+			   board[i][j] = 9;
+		   }
+	   }
+}
  
  public static void main(String[] args) throws IOException {
   Player rp=new Player();
@@ -57,5 +82,7 @@ public class Player {
   rp.processInput();
   rp.processInput();
  }
+ 
 }
+ 
 
